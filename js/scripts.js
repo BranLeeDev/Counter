e519da561543e2b4daac8c6a00c573ee36095e97
Brandon <name.brandon.aguero@gmail.com>
2023-08-28 10:07:21 -0500

    Implementación de localstorage

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,42 @@ const buttons = document.querySelectorAll(".counter__buttons");
 // Counter value
 let value = +counterValue.textContent;
 
+const valueCounterStorage = window.localStorage.getItem("valueCounter");
+const styleColorStorage = window.localStorage.getItem("styleColor");
+if (valueCounterStorage && styleColorStorage) {
+  counterValue.textContent = valueCounterStorage;
+  value = +valueCounterStorage;
+  counterValue.style.color = styleColorStorage;
+}
+
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const styles = e.target.classList[1];
     if (styles === "counter__button--increase") {
       ++value;
+      window.localStorage.setItem("valueCounter", value);
     } else if (styles === "counter__button--decrease") {
       --value;
+      window.localStorage.setItem("valueCounter", value);
     } else if (styles === "counter__button--reset") {
       value = 0;
+      window.localStorage.setItem("valueCounter", value);
     }
 
     counterValue.textContent = value;
 
     if (value < 0) {
-      counterValue.style.color = "#ff0000";
+      const color = "#ff0000";
+      counterValue.style.color = color;
+      window.localStorage.setItem("styleColor", color);
     } else if (value > 0) {
-      counterValue.style.color = "#008000";
+      const color = "#008000";
+      counterValue.style.color = color;
+      window.localStorage.setItem("styleColor", color);
     } else {
-      counterValue.style.color = "#102a42";
+      const color = "#102a42";
+      counterValue.style.color = color;
+      window.localStorage.setItem("styleColor", color);
     }
   });
 });
